c0df28a7d53459c26e3988b2bdc4a6aa0175dc40
Souvik Dey <souvik001dey@gmail.com>
2024-07-29 19:03:43 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ const app = express();
 config({path: "./config/config.env"});
 
 app.use(cors({
-     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+     origin: "*",
      methods: ["GET", "POST", "PUT", "DELETE"],
      credentials: true,
 }))
