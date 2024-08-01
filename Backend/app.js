6d496aa9dd4a80910f9b076f316141c8865ce445
Souvik Dey <93041744+souvik987@users.noreply.github.com>
2024-08-01 17:11:50 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,10 +16,17 @@ const app = express();
 config({path: "./config/config.env"});
 
 app.use(cors({
-    origin: '*',
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
     methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+}));
+
+app.options('*', cors({
+    origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use(cookieParser());
